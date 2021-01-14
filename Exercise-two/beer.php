cbdf63ae2e39a8907483c5e69400d5b780682aea
Fran1409 <fran_andries@hotmail.com>
2021-01-14 15:49:27 +0100

    Solution exercise two

---
+++ Exercise-two/beer.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+class Beer extends Beverage 
+{
+    public $name;
+    public $alcoholpercentage;
+
+    public function __construct(string $name, float $alcoholpercentage, string $color, float $price, string $temperature = 'cold') {
+        parent::__construct($color, $price, $temperature);
+
+        $this->name = $name;
+        $this->alcoholpercentage = $alcoholpercentage;
+    }
+
+    public function getAlcoholpercentage() :string {
+        return 'The alcoholpercentage is '. $this->alcoholpercentage.'%.';
+    }
+}
+
